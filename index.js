8d2bc30b91028c4a617c59b44cf87693d7f1b0bc
Mohit Singh Tomar <mohitsingh2004624@gmail.com>
2024-12-20 13:50:24 +0530

    Complete

---
+++ index.js
@@ -0,0 +1,103 @@
+// const accessKey = "RumO81BZnrqLly-JtfCYNCUZz3eaxej3Lr4vg2SiavM"
+
+// const formEl = document.querySelector("from")
+// const searchInputEl = document.getElementById("search-input")
+// const searchResultsEl = document.querySelector(".search-result")
+// const showMoreButton = document.getElementById("show-more-button")
+
+// let inputData = ""
+// let page = 1
+
+// async function searchImages(){
+//     inputData = searchInputEl.value
+//     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+//     const response = await fetch(url)
+//     const data = await response.json()
+//     if(page === 1){
+//         searchResultsEl.innerHTML = ""
+//     }
+
+//     const results = data.results
+//     results.map((result) => {
+//         const imageWrapper = document.createElement("div");
+//         imageWrapper.classList.add("search-result");
+//         const image = document.createElement("img");
+//         image.src = result.urls.small;
+//         image.alt = result.alt_description;
+//         const imageLink = document.createElement("a");
+//         imageLink.href = result.links.html;
+//         imageLink.target = "_blank";
+//         imageLink.textContent = result.alt_description;
+
+//         imageWrapper.appendChild(image);
+//         imageWrapper.appendChild(imageLink);
+//         searchResultsEl.appendChild(imageWrapper);
+//     });
+
+//     page++
+
+//     if(page > 1){
+//         showMoreButton.style.display = "block"
+//     }
+// }
+// formEl.addEventListener("submit", (event) =>{
+//     event.preventDefault();
+//     page = 1
+//     searchImages();
+// });
+
+const accessKey = "RumO81BZnrqLly-JtfCYNCUZz3eaxej3Lr4vg2SiavM";
+
+const formEl = document.querySelector("form");
+const searchInputEl = document.getElementById("search-input");
+const searchResultsEl = document.querySelector(".search-results");
+const showMoreButtonEl = document.getElementById("show-more-button");
+
+let inputData = "";
+let page = 1;
+
+async function searchImages() {
+    inputData = searchInputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    if (page === 1) {
+        searchResultsEl.innerHTML = "";
+    }
+
+    const results = data.results;
+
+    results.map((result) => {
+        const imageWrapper = document.createElement("div");
+        imageWrapper.classList.add("search-result");
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
+
+        imageWrapper.appendChild(image);
+        imageWrapper.appendChild(imageLink);
+        searchResultsEl.appendChild(imageWrapper);
+    });
+
+    page++;
+
+    if (page > 1) {
+        showMoreButtonEl.style.display = "block";
+    }
+}
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    page = 1;
+    searchImages();
+});
+
+showMoreButtonEl.addEventListener("click", () => {
+    searchImages();
+});
